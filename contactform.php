70cbf4ef88d1aae22900027c17b9b9939263e732
brookecheney <brookecheney@hotmail.com>
2020-05-01 13:16:39 -0700

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,27 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "brookecheney@hotmail.com";
+    $headers = "From: My ".$mailFrom;
+    $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+
+
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
+
+}
+
+
+
+
+
+
+
+
+?>
